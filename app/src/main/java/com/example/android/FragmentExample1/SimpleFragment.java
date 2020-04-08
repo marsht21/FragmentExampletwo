042b3a69e2148b3aac05d56e87d1da8552a4f6fe
Tucker Marsh <marsht21@hanover.edu>
2020-04-07 20:19:22 -0400

    Finished challenge problem

--- app/src/main/java/com/example/android/FragmentExample1/SimpleFragment.java
+++ app/src/main/java/com/example/android/FragmentExample1/SimpleFragment.java
@@ -1,59 +1,23 @@
 package com.example.android.FragmentExample1;
 
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SimpleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SimpleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     private static final int YES = 0;
     private static final int NO = 1;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public SimpleFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SimpleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SimpleFragment newInstance(String param1, String param2) {
-        SimpleFragment fragment = new SimpleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,7 +25,7 @@ public class SimpleFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
-
+        final RatingBar ratingBar = rootView.findViewById(R.id.rating_bar_song);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -80,6 +44,14 @@ public class SimpleFragment extends Fragment {
                         // Do nothing.
                         break;
                 }
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                String myRating = (getString(R.string.my_rating) + ratingBar.getRating());
+                Toast.makeText(getContext(), myRating, Toast.LENGTH_SHORT).show();
             }
         });
         return rootView;
